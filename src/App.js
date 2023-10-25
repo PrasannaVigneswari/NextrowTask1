bf6cdeac58a649b5c92581b2e8ee8205e2e157d4
PrasannaVigneswari <146935812+PrasannaVigneswari@users.noreply.github.com>
2023-10-25 17:05:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,30 +6,21 @@ import SignupForm from './Components/SignupForm/SignupForm';
 function App() {
   // Track the user's name
   const [name, setName] = useState('');
-
   // Function to handle login
   const handleLogin = (username) => {
     setName(username);
   };
-
-  return (
+return (
     <div className="App">
       <Router>
         <Routes>
-          <Route
-            path="/"
-            element={<SignupForm handleLogin={handleLogin} />}
-          />
-          <Route
-            path="/Todo-list"
-            element={<Todolist name={name} />}
-          />
+          <Route path="/" element={<SignupForm handleLogin={handleLogin} />} />
+          <Route path="/Todo-list" element={<Todolist name={name} />} />
         </Routes>
       </Router>
     </div>
   );
 }
-
 export default App;
 
 
