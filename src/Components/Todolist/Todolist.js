a35d24b2e7ec9d32d7c55c583b1a2a59dc3a7ba1
PrasannaVigneswari <146935812+PrasannaVigneswari@users.noreply.github.com>
2023-10-25 16:52:15 +0530

    Update Todolist.js

--- src/Components/Todolist/Todolist.js
+++ src/Components/Todolist/Todolist.js
@@ -1,56 +1,106 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import { AddCircle } from '@mui/icons-material';
 import { useLocation } from 'react-router-dom';
-import './Todolist.css';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import { AddCircle } from '@mui/icons-material';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const Todolist = () => {
-  const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState('');
-
+  
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const name = searchParams.get('name');
  
- const addTodo = () => {
-    if (newTodo) {
-      setTodos([...todos, newTodo]);
-      setNewTodo('');
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+  const [error, setError] = useState('');
+
+  // Load tasks from local storage on component mount
+  useEffect(() => {
+    if (localStorage.getItem("localTasks")) {
+      const storedList = JSON.parse(localStorage.getItem("localTasks"));
+      setTodos(storedList);
+    }
+  }, []);
+
+  // Add a new task with validation for an empty task
+  const addTodo = () => {
+    if (newTodo.trim() === '') {
+      setError('Task cannot be empty');
+      return;
+    }
+
+    const newTask = { id: new Date().getTime().toString(), title: newTodo };
+    setTodos([...todos, newTask]);
+    localStorage.setItem("localTasks", JSON.stringify([...todos, newTask]));
+    setNewTodo("");
+    setError("");
+  };
+
+  const toggleCompletion = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].completed = !updatedTodos[index].completed;
+    setTodos(updatedTodos);
+    localStorage.setItem("localTasks", JSON.stringify(updatedTodos));
+  };
+
+  // Remove a task with confirmation
+  const deleteTodo = (index) => {
+    const confirmation = window.confirm('Are you sure you want to delete?');
+    if (confirmation) {
+      const updatedTodos = [...todos];
+      updatedTodos.splice(index, 1);
+      setTodos(updatedTodos); 
+      localStorage.setItem("localTasks", JSON.stringify(updatedTodos));
+    } 
+  };
+
+  //Clear tasks with confirmation
+  const handleClear = () => {
+    const confirmation = window.confirm("Are you sure you want to clear?");
+    if (confirmation) {
+      setTodos([]);
+      localStorage.removeItem('localTasks');
     }
   };
 
-  const removeTodo = (index) => {
-    const updatedTodos = [...todos];
-    updatedTodos.splice(index, 1);
-    setTodos(updatedTodos);
-  };
 
   return (
-    
-    <div className="todo-container">
-       <p className='text-4xl font-semibold mt-10 text-center'>Welcome, {name}!</p> 
-       <h1 className="text-4xl font-semibold mt-10 text-center" >Todo List</h1>
-       
-       <div className="flex gap-4 mt-10 align-center justify-center">
-         <TextField label="Add a new todo" variant="outlined" value={newTodo}
-          onChange={(e) => setNewTodo(e.target.value)} />
-         <Button variant="contained" color="primary"  className=" hover:bg-cyan-400" startIcon={<AddCircle />}
-          onClick={addTodo} >Add </Button>
+    <div className="p-10 h-screen">
+      <h1 className='text-4xl font-serif font-semibold text-sky-800 text-center'>Hi, {name}</h1>
+      <h1 className="text-4xl text-center font-serif mt-8">Todos</h1>
+      <h2 className="text-3xl mt-10">Create Task</h2>
+      
+      <div className="mt-10">
+        <TextField label="Add a new todo" variant="outlined" fullWidth value={newTodo}
+          onChange={(e) => {setNewTodo(e.target.value);
+          setError('');
+          }} />
+        <Button variant="contained" className="hover:bg-cyan-400 primary" sx={{ marginTop: "20px" }}
+          startIcon={<AddCircle />} onClick={addTodo} > Add </Button>
+        {error && <p className="text-red-500 mt-2">{error}</p>}
       </div>
 
-      <ul className="todo-list">
+      <h4 className="text-3xl mt-10 mb-5">My Tasks</h4>
+      
+      <List>
         {todos.map((todo, index) => (
-          <li key={index} className="flex items-center justify-between bg-cyan-100 rounded p-2 mb-2 m-3 shadow-md">
-            {todo}
-            <button
-              className="text-red-500"
-              onClick={() => removeTodo(index)} > Remove </button>
-            </li>))}
-      </ul>
-     
+          <ListItem key={todo.id} className={`flex items-center bg-cyan-50 rounded mt-5 shadow-md`} >
+            <Checkbox onClick={() => toggleCompletion(index)} checked={todo.completed} />
+            <span >{todo.title}</span>
+            <div className="ml-auto">
+              <IconButton onClick={() => deleteTodo(index)}> <DeleteIcon /> </IconButton>
+            </div>
+          </ListItem>
+        ))}
+      </List>
+      <Button variant="outlined" sx={{ marginTop: "20px" }} 
+       onClick={()=>handleClear()}> clear</Button>
     </div>
   );
-};
-
+}
 export default Todolist;
