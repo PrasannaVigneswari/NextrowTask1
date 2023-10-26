74d201286d67087f22d57786928e990aa95088eb
PrasannaVigneswari <146935812+PrasannaVigneswari@users.noreply.github.com>
2023-10-26 14:44:39 +0530

    Update Todolist.js

--- src/Components/Todolist/Todolist.js
+++ src/Components/Todolist/Todolist.js
@@ -14,10 +14,10 @@ const Todolist = () => {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const name = searchParams.get('name');
+  const [idCounter, setIdCounter] = useState(0);
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState('');
   const [error, setError] = useState('');
-
   // Load tasks from local storage on component mount
   useEffect(() => {
     if (localStorage.getItem("localTasks")) {
@@ -25,27 +25,27 @@ const Todolist = () => {
       setTodos(storedList);
     }
   }, []);
-
   // Add a new task with validation for an empty task
   const addTodo = () => {
     if (newTodo.trim() === '') {
       setError('Task cannot be empty');
       return;
     }
-  const newTask = { id: new Date().getTime().toString(), title: newTodo };
+    const newTask = { //current value of the ID counter as the ID
+      id: idCounter, title: newTodo,
+    };
     setTodos([...todos, newTask]);
-    localStorage.setItem("localTasks", JSON.stringify([...todos, newTask]));
-    setNewTodo("");
-    setError("");
+    localStorage.setItem('localTasks', JSON.stringify([...todos, newTask]));
+    setNewTodo('');
+    setError(''); 
+    setIdCounter(idCounter + 1); // Increment the ID counter for the next task
   };
-
   const toggleCompletion = (index) => {
     const updatedTodos = [...todos];
     updatedTodos[index].completed = !updatedTodos[index].completed;
     setTodos(updatedTodos);
     localStorage.setItem("localTasks", JSON.stringify(updatedTodos));
   };
-
   // Remove a task with confirmation
   const deleteTodo = (index) => {
     const confirmation = window.confirm('Are you sure you want to delete?');
@@ -56,7 +56,6 @@ const Todolist = () => {
       localStorage.setItem("localTasks", JSON.stringify(updatedTodos));
     } 
   };
-
   //Clear tasks with confirmation
   const handleClear = () => {
     const confirmation = window.confirm("Are you sure you want to clear?");
@@ -65,7 +64,6 @@ const Todolist = () => {
       localStorage.removeItem('localTasks');
     }
   };
-
   return (
     <div className="p-10 h-screen">
       <h1 className='text-4xl font-serif font-semibold text-sky-800 text-center'>Hi, {name}</h1>
@@ -81,21 +79,22 @@ const Todolist = () => {
           startIcon={<AddCircle />} onClick={addTodo} > Add </Button>
         {error && <p className="text-red-500 mt-2">{error}</p>}
       </div>
+
       <h4 className="text-3xl mt-10 mb-5">My Tasks</h4>
-      <List>
+       <List>
         {todos.map((todo, index) => (
-        <ListItem key={todo.id} className={`flex items-center bg-cyan-50 rounded mt-5 shadow-md`} >
-        <Checkbox onClick={() => toggleCompletion(index)} checked={todo.completed} />
-        <span >{todo.title}</span>
-        <div className="ml-auto">
-        <IconButton onClick={() => deleteTodo(index)}> <DeleteIcon /> </IconButton>
-        </div>
-       </ListItem>
+          <ListItem key={todo.id} className={`flex items-center bg-cyan-50 rounded mt-5 shadow-md max-w-screen-sm`} >
+            <Checkbox onClick={() => toggleCompletion(index)} checked={todo.completed} />
+            <span >{todo.title}</span>
+            <div className="ml-auto">
+              <IconButton onClick={() => deleteTodo(index)}> <DeleteIcon /> </IconButton>
+            </div>
+          </ListItem>
         ))}
-    </List>
-    <Button variant="outlined" sx={{ marginTop: "20px" }} 
+      </List>
+      <Button variant="outlined" sx={{ marginTop: "20px" }} 
        onClick={()=>handleClear()}> clear</Button>
-  </div>
+    </div>
   );
 }
 export default Todolist;
