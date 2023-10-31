2f1521051e0102e74433c16c55822410ca621ac6
PrasannaVigneswari <146935812+PrasannaVigneswari@users.noreply.github.com>
2023-10-31 10:52:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,7 @@ import Todolist from './components/todolist/Todolist';
 import SignupForm from './components/signupform/SignupForm';
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<SignupForm />} />
-          <Route path="/Todo-list" element={<Todolist />} />
-  // Track the user's name
-  const [name, setName] = useState('');
-  // Function to handle login
-  const handleLogin = (username) => {
-    setName(username);
-  };
+  
 return (
     <div className="App">
       <Router>
