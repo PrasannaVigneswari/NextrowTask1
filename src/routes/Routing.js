ade68a4b69241e3750a7e71c805396e0aa29fc0c
PrasannaVigneswari <prasannavigneswari@nextrow.com>
2023-10-31 15:19:34 +0530

    changes in folder names

--- src/routes/Routing.js
+++ src/routes/Routing.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import SignupForm from "../components/signupform/SignupForm";
-import Todolist from "../components/todolist/Todolist";
+import SignupForm from "../Components/SignupForm/SignupForm";
+import Todolist from "../Components/Todolist/Todolist";
 
 const Routing = () => {
   return (
