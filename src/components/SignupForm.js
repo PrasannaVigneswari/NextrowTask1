c373045e4b1887634b5d98081dd2344a10e8ccae
PrasannaVigneswari <prasannavigneswari@nextrow.com>
2023-11-07 21:59:44 +0530

    Add Navigation sidebar, user profile update feature with validations and dialogs

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import Avatar from "@mui/material/Avatar";
+import { Button, Avatar, Typography, Grid, TextField } from "@mui/material";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Grid from "@mui/material/Grid";
 import { useNavigate } from "react-router-dom";
-import "./SignupForm.css";
+import { validateEmail, validatePassword } from "../utils/validation";
+import { useUser } from "../utils/UserContext";
+import useLocalStorage from "../utils/useLocalStorage";
+
 
 const SignupForm = () => {
+
+  const { setUser } = useUser();
+  const [storedUser, setStoredUser] = useLocalStorage('user', null);
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
@@ -31,11 +33,6 @@ const SignupForm = () => {
     });
   };
 
-  const validateEmail = (email) => {
-    const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i; //emailRegex is a regular expression that matches common email address patterns.
-    // use .test(email) method checks if the provided email matches the regular expression pattern
-    return emailRegex.test(email);
-  }; // If the email matches the pattern, test returns true Otherwise, it returns false.
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -45,10 +42,25 @@ const SignupForm = () => {
     if (!validateEmail(formData.email)) {
       //check Email adress is valid or not
       setValidationMessages({ email: "Invalid email address" }); // Set an error message for the email field.
+    } else if (!validatePassword(formData.password)) {
+      setValidationMessages({
+        password:
+          "Password should have at least 1 capital letter, 1 numeric, and 1 special character",
+      });
     } else {
+      setUser({
+        firstName: formData.firstName,
+        lastName: formData.lastName,
+        email: formData.email,
+      });
+      setStoredUser({
+        firstName: formData.firstName,
+        lastName: formData.lastName,
+        email: formData.email,
+      });
       history(`/Todo-list?name=${formData.firstName}`);
-      // If the email address is valid, navigate to the Todo-list page with the user's first name.
-    }
+    }   
+      
   };
 
   return (
@@ -119,6 +131,8 @@ const SignupForm = () => {
             sx={{ m: 1, width: "43ch" }}
             value={formData.password}
             onChange={handleChange}
+            error={validationMessages.password && isSubmitted}
+            helperText={validationMessages.password}
           />
 
           <Button
